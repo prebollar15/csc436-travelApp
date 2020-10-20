629533f4cf57aba458081c9841a6b1bf0095aa8c
Pedro Rebollar <prebolla15@gmail.com>
2020-10-19 22:36:10 -0500

    flight html page w/o bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Pedro Rebollar';
+  title = 'Flight Hack';
+  creator = 'Pedro Rebollar';
 }
